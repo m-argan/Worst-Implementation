250248dd8c12840775823bba396033dbe7c5e51f
m-argan <159178053+m-argan@users.noreply.github.com>
2025-02-17 17:40:02 -0500

    Update Contractor.java
    removed spaces

--- Contractor.java
+++ Contractor.java
@@ -4,17 +4,17 @@ public class E1 implements Employee
 {private String a;
 private int b;
 private int c;	
-public E1(String a1, int b1, int c1)
-{this.a = a1;
-this.b = b1;
-this.c = c1;}
-public Employee createNew(String name, int rate, int hours)
-{return new E1(a, b, c);}
+public E1(String a1,int b1,int c1)
+{this.a=a1;
+this.b=b1;
+this.c=c1;}
+public Employee createNew(String name,int rate,int hours)
+{return new E1(a,b,c);}
 public double sparkle_dash()
-{return (b * c);}
+{return (b*c);}
 @Override
 public void pumpkinAss(int hours) 
-{this.hours += hours;}
+{this.hours+=hours;}
 public String ApPlEsAucE(){return a;}
 public int xyz(){return b;}
 public int late(){return c;}}
